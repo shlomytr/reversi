55a18bcc6d18b5daa7ed0405538e28b270ea9265
omeryaso <omeryaso@gmail.com>
2017-11-28 16:13:01 +0200

    spilt the structure of the directories a few changes in the cmake added 2 classes for testing

---
+++ test/board_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by omer on 28/11/17.
+//
+
+#include <gtest/gtest.h>
+#include "../include/Game.h"
+
+
+TEST(GameTest, HandlePositiveInput) {
+
+    EXPECT_EQ(1,1);
+}
---
+++ test/gtest_main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by omer on 28/11/17.
+//
+
+#include <gtest/gtest.h>
+GTEST_API_ int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
