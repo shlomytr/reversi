5d833677c9dc5ef8d9a59bc43808321b95b62c19
Shlomy Trajber <shlomytra@gmail.com>
2018-01-03 22:30:32 +0200

    Added documantation in GameMode

--- include/GameMode.h
+++ include/GameMode.h
@@ -13,11 +13,36 @@ class GameMode {
 
 public:
 
+    /**
+     * @name : sendToServer
+     * @parameters : a pair of players , the game's Logic, Board, and an instance of HumanPlayer and Client
+     * @return : the function is used if the player wish to play online and updates the player's pair accordingly
+     *          furthermore, the function send and gets data from the server
+     **/
+
     void sendToServer (pair <Player *,Player *> &players, GameLogic &l, Board &board, Client &c, HumanPlayer &h);
+
+    /**
+     * @name : chooseGameMode
+     * @parameters : an instance of the game's Logic, Board, Printer, Client and HumanPlayer
+     * @return : the function returns a pair of the player's wished game mode
+     **/
 
     pair <Player *,Player *>  chooseGameMode(GameLogic &l, Board &board, Printer &p, Client &c, HumanPlayer &h);
 
+    /**
+     * @name : PrintMenu
+     * @parameters : no parameters
+     * @return : the function prints a message of welcome in the online play section
+     **/
+
     void PrintMenu();
+
+    /**
+     * @name : commandTranslator
+     * @parameters : a string of the command that wished to be used
+     * @return : the function change the string to an known int and returns it
+     **/
 
     int commandTranslator(string command);
 };
